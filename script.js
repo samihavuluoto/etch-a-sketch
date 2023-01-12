ca20ef773e1ae100ca9c5e42865ad928dd31d67d
Sami Havuluoto <sami.havuluoto@gmail.com>
2023-01-12 14:28:11 +0000

    First commit
    Created initial index.html, script.js, style.css files. Added
    masterContainer in the html, and used JS for generating the 16x16
    square divs.

---
+++ script.js
@@ -0,0 +1,7 @@
+const masterContainer = document.querySelector("#masterContainer");
+
+
+for (let i = 0; i < 16 ; i++){
+    const square = document.createElement("div");
+    masterContainer.appendChild(square);
+}
