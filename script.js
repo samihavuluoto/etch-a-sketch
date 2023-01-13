dd977b33a5e10c937837a1a6be0673d49cb79a29
Sami Havuluoto <sami.havuluoto@gmail.com>
2023-01-13 16:48:43 +0000

    Add initial grid and hover effect
    Added JS code to create the grid out of divs. Added CSS to the
    masterContainer to create a flexbox. Added the hover effect to the div
    grid.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const masterContainer = document.querySelector("#masterContainer");
+let grid = 16 * 16;
 
+// Relative size for a square in comparison to the masterContainer. Multiplication by 100 to get a percentage.
+let size = 1 / 16 * 100;
 
-for (let i = 0; i < 16 ; i++){
+for (let i = 0; i < grid ; i++){
     const square = document.createElement("div");
+    square.style.height = size + "%";
+    square.style.width = size + "%";
+    square.style.flex = "auto";
+    square.style.flexShrink = 0;
+    square.style.margin = "0px";
+    square.onmouseover = () => square.classList.add("hover");
     masterContainer.appendChild(square);
 }
