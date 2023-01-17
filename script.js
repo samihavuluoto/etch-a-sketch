7b2310066e6f1a54e14f1860fff7d039ac995673
Sami Havuluoto <sami.havuluoto@gmail.com>
2023-01-17 19:24:24 +0000

    Add button for resizing the grid
    Added a button that prompts the user to resize the grid.

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+
 const masterContainer = document.querySelector("#masterContainer");
-let grid = 16 * 16;
 
-// Relative size for a square in comparison to the masterContainer. Multiplication by 100 to get a percentage.
-let size = 1 / 16 * 100;
+const button = document.querySelector("#resize");
 
-for (let i = 0; i < grid ; i++){
-    const square = document.createElement("div");
-    square.style.height = size + "%";
-    square.style.width = size + "%";
-    square.style.flex = "auto";
-    square.style.flexShrink = 0;
-    square.style.margin = "0px";
-    square.onmouseover = () => square.classList.add("hover");
-    masterContainer.appendChild(square);
+button.addEventListener("click", resizeSquares);
+
+function removeChildren(){
+    const children = document.querySelectorAll("#masterContainer > div");
+    children.forEach(element => element.remove());
+}
+
+
+function resizeSquares() {
+    let b = prompt("How many rows of squares would you like?");
+    
+    let a = b > 100 ? 100 : b;
+
+    removeChildren()
+
+    let grid = a * a;
+
+    // Relative size for a square in comparison to the masterContainer. Multiplication by 100 to get a percentage.
+    let size = 1 / a * 100;
+
+    for (let i = 0; i < grid ; i++){
+        const square = document.createElement("div");
+        square.style.height = size + "%";
+        square.style.width = size + "%";
+        square.style.flex = "auto";
+        square.style.flexShrink = 0;
+        square.style.margin = "0px";
+        square.onmouseover = () => square.classList.add("hover");
+        masterContainer.appendChild(square);
+    }
 }
